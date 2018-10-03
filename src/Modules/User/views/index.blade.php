5d49d414a3707fd5de24a2282fd6525bb325727d
Armand Grundmanis <agrundmanis@inbox.lv>
2018-10-03 23:59:46 +0300

    removed container from user views

--- src/Modules/User/views/index.blade.php
+++ src/Modules/User/views/index.blade.php
@@ -1,41 +1,35 @@
 @extends('laracms.dashboard::layouts.app', ['page' => 'Laracms Users'])
 
 @section('content')
-
-    <div class="content">
-        <div class="container-fluid">
-
-            <div class="form-group">
-                <a class="btn btn-success" href="{{ route('laracms.users.create') }}">Create</a>
-            </div>
-            <div class="table-responsive">
-                <table class="table table-striped">
-                    <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>name</th>
-                        <th>e-mail</th>
-                        <th></th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($users as $user)
-                        <tr>
-                            <td>{{ $user->id }}</td>
-                            <td>{{ $user->name }}</td>
-                            <td>{{ $user->email }}</td>
-                            <td>
-                                <a href="{{ route('laracms.users.edit', $user->id) }}">Edit</a>
-                                |
-                                <a onclick="return confirm('Are you sure?')"
-                                   href="{{ route('laracms.users.destroy', $user->id) }}">Delete</a>
-                            </td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-                {{ $users->links() }}
-            </div>
-        </div>
+    <div class="form-group">
+        <a class="btn btn-success" href="{{ route('laracms.users.create') }}">Create</a>
+    </div>
+    <div class="table-responsive">
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th>#</th>
+                <th>name</th>
+                <th>e-mail</th>
+                <th></th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($users as $user)
+                <tr>
+                    <td>{{ $user->id }}</td>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>
+                        <a href="{{ route('laracms.users.edit', $user->id) }}">Edit</a>
+                        |
+                        <a onclick="return confirm('Are you sure?')"
+                           href="{{ route('laracms.users.destroy', $user->id) }}">Delete</a>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+        {{ $users->links() }}
     </div>
 @endsection
