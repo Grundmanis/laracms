325380eb3127f5f18995b0384593deaaf15f172e
Armands Grundmanis <grundmanweb@gmail.com>
2019-07-22 04:41:57 +0300

    update breadcrumbs template

--- src/Modules/Breadcrumbs/views/links.blade.php
+++ src/Modules/Breadcrumbs/views/links.blade.php
@@ -1,11 +1,13 @@
-<ol class="breadcrumb" style="margin-bottom: 0;">
-    @foreach($segments as $key => $segment)
-        <li>
-            @if ($segmentCount != $key)
-                <a class="text-info" href="{{ $url .= $segment . '/' }}">{{ ucfirst($segment) }}</a>&nbsp;/&nbsp;
-            @else
-                 {{ ucfirst($segment) }}
-            @endif
-        </li>
-    @endforeach
-</ol>
+@if(count($segments))
+    <ol class="breadcrumb" style="margin-bottom: 0;">
+        @foreach($segments as $key => $segment)
+            <li>
+                @if ($segmentCount != $key)
+                    <a class="text-info" href="{{ $url .= $segment . '/' }}">{{ ucfirst($segment) }}</a>&nbsp;/&nbsp;
+                @else
+                    {{ ucfirst($segment) }}
+                @endif
+            </li>
+        @endforeach
+    </ol>
+@endif
